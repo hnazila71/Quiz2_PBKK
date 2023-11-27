b0b3c7f15f53d1b726828aaec96f82656abad45f
unknown <hnazila71@gmail.com>
2023-11-27 09:00:42 +0700

    hasan

--- app/Views/bioskop.php
+++ app/Views/bioskop.php
@@ -4,6 +4,23 @@
     <meta charset="UTF-8">
     <title>Cinematix - Aplikasi Bioskop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        body {
+            margin-bottom: 60px; /* Tinggi footer */
+        }
+
+        footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px; /* Sesuaikan dengan tinggi footer */
+        }
+    </style>
 </head>
 <body>
     <header>
